38171e28c1a1be6a78a58c3ba91c6f6e974887a1
Nataliia <Natasha.bulo4nikova@yandex.ru>
2023-01-31 11:31:21 +0300

    basic functionality

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Word Composition',
       theme: ThemeData(
         primarySwatch: Colors.grey,
--- lib/screens/game.dart
+++ lib/screens/game.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:word_composition/style/colors.dart';
 
 class GameScreen extends StatefulWidget {
-  const GameScreen({super.key});
+  GameScreen({super.key});
 
   @override
   State<GameScreen> createState() => _GameScreenState();
@@ -10,12 +11,145 @@ class GameScreen extends StatefulWidget {
 class _GameScreenState extends State<GameScreen> {
   @override
   Widget build(BuildContext context) {
+    // data
+    const String word = 'музыка';
+    List<String> letters = word.split('');
+
     return Scaffold(
-      appBar: AppBar(),
+      backgroundColor: AppColors.mainColor,
+      appBar: AppBar(
+        title: const Text(
+          'Word Composition',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: AppColors.mainColor,
+      ),
       body: SingleChildScrollView(
+        padding: const EdgeInsets.all(15),
+
+        // main column
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            
+            // words
+            Container(
+              height: 200,
+              child: GridView.builder(
+                scrollDirection: Axis.vertical,
+                gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                  maxCrossAxisExtent: 50,
+                  childAspectRatio: 1 / 1,
+                  crossAxisSpacing: 20,
+                  mainAxisSpacing: 20,
+                ),
+                itemCount: letters.length,
+                itemBuilder: ((context, index) {
+                  return Draggable(
+                    feedback: Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: Colors.grey,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                      ),
+                      child: Center(
+                        child: Text(
+                          letters[index],
+                          style: TextStyle(
+                              color: AppColors.mainColor, fontSize: 25),
+                        ),
+                      ),
+                    ),
+                    childWhenDragging: Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: Colors.grey.withOpacity(0.3),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                      ),
+                      child: Center(
+                        child: Text(
+                          letters[index],
+                          style: TextStyle(
+                              color: AppColors.mainColor, fontSize: 25),
+                        ),
+                      ),
+                    ),
+                    child: Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: Colors.grey,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                      ),
+                      child: Center(
+                        child: Text(
+                          letters[index],
+                          style: TextStyle(
+                              color: AppColors.mainColor, fontSize: 25),
+                        ),
+                      ),
+                    ),
+                  );
+                }),
+              ),
+            ),
+
+            // receive container
+            DragTarget(
+              builder: ((context, candidateData, rejectedData) => Container(
+                    width: double.maxFinite,
+                    height: 70,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10),
+                      ),
+                      color: Colors.grey,
+                    ),
+                    child: Row(
+                      children: [],
+                    ),
+                  )),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+
+            // check button
+            ElevatedButton(
+              onPressed: () {},
+              child: Text(
+                'Проверить',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green,
+                foregroundColor: Colors.white,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 10,
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
