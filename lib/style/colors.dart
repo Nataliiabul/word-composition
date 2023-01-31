6fc207360b72078ead3e86689fe4f089f3f6b15d
Nataliia <Natasha.bulo4nikova@yandex.ru>
2023-01-31 09:34:39 +0300

    basic structure

---
+++ lib/style/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static final Color mainColor = Color(0xFF3E4E50);
+  static final Color firstColor = Color(0xFFFACFAD);
+  static final Color secondColor = Color(0xFFF8BD7F);
+}
